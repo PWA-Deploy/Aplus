049b980713dfe15f6616909e63be7f1fb2c87acb
PWA-Deploy <56715770+PWA-Deploy@users.noreply.github.com>
2019-10-18 04:22:42 -0400

    Initial commit

---
+++ precache-manifest.21db1a185c3473c42731901e5cc16b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "13a5a47200b88ab162c9",
+    "url": "/js/chunk-vendors.634093dc.js"
+  },
+  {
+    "revision": "bdee0ad3417c5f9c4553",
+    "url": "/js/app.7ad290d0.js"
+  },
+  {
+    "revision": "3144a553c46fdf8c5d2664ad8dabbd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bdee0ad3417c5f9c4553",
+    "url": "/css/app.0928d202.css"
+  }
+];
